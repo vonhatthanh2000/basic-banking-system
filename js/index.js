040816495dfc55798c7a8d20f7dad3637951c7d2
nthanh02 <nthanhatee@gmail.com>
2022-12-11 01:45:49 +0700

    feat: update function bell

--- js/index.js
+++ js/index.js
@@ -2,12 +2,15 @@ const bellClick = document.querySelector(".notify-header");
 const container = document.querySelector(".notify-inner-container");
 const closeBtn = document.querySelector(".button-close");
 
+
 const handleToggle = () => {
+  $('ion-icon').removeClass( "bell-icon");
   if (container?.classList.value.split(" ").includes("show")) {
     container.classList.add("notShow");
     container.classList.remove("show");
   } else {
     container.classList.remove("notShow");
+
     container.classList.add("show");
   }
 };
@@ -19,3 +22,5 @@ bellClick?.addEventListener("click", (e) => {
 closeBtn?.addEventListener("click", () => {
   handleToggle();
 });
+
+
