957ddd9a4132b77a01ada08859653fff1e57499f
datphamcode295 <vandatpro2000@gmail.com>
2022-12-11 10:56:10 +0700

    chore: total admin in dashboard

--- pages/Dashboard.php
+++ pages/Dashboard.php
@@ -9,10 +9,16 @@
         }
 
     // ===========All Dashboard==========
-        $sql = mysqli_query($con,"SELECT * FROM employe");
+        $sql = mysqli_query($con,"SELECT * FROM users WHERE role='employee'");
         $total_employes = mysqli_num_rows($sql);
         if($total_employes < 10){
             $total_employes = '0'.$total_employes;
+        }
+
+        $sql = mysqli_query($con,"SELECT * FROM users WHERE role='admin'");
+        $total_admin = mysqli_num_rows($sql);
+        if($total_admin < 10){
+            $total_admin = '0'.$total_admin;
         }
     // =========X==All Dashboard==X========
    
@@ -20,14 +26,26 @@
     <!-- -----------Dashboard------------ -->
         <?php include '../components/User_Name.php' ?>
         <div class="container mt-5">
-            <div class="d-flex justify-content-center">
+            <div class="container mt-5">
+            <div class="row">
+                <div class="col-xl-6">
                     <div class="card text-dark bg-light mb-3" style="max-width: 27rem;">
                         <div class="card-body text-center">
-                            <h4 class="card-title text-uppercase text-primary">Total Employees</h4>
+                            <h4 class="card-title text-uppercase text-primary">Total Employee</h4>
                             <h1 class="card-text text-success"><?php echo $total_employes; ?></h1>
                         </div>
+                    </div>
+                </div>
+                <div class="col-xl-6">
+                    <div class="card text-dark bg-light mb-3" style="max-width: 27rem;">
+                        <div class="card-body text-center">
+                            <h4 class="card-title text-uppercase text-primary">Total Admin</h4>
+                            <h1 class="card-text text-success"><?php echo $total_admin; ?></h1>
+                        </div>
+                    </div>
                 </div>
             </div>
+        </div>
         </div>
     <!-- -----------Dashboard------------ -->
 <?php
